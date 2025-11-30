20f1584f436ac42c2d5d7f812641829aa577b96d
DrollerBubble59 <discordude89@gmail.com>
2025-11-30 00:15:28 -0500

    adding files

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final double DEADBAND = 0.05;
+  }
+
+  public static final String kLimelightName = "limelight";
+
+  public static enum Reef {
+    left,
+    right,
+    forward;
+  }
+
+
+  public static final class PathPlannerConstants {
+
+    public static final PathConstraints testingConstraints = new PathConstraints(
+        Units.feetToMeters(1.5), 2.0,             
+        Units.degreesToRadians(50), Units.degreesToRadians(300));
+
+    public static final PathConstraints slowConstraints = new PathConstraints(
+        Units.feetToMeters(3.5), 4.0,             
+        Units.degreesToRadians(100), Units.degreesToRadians(720));
+
+    public static final PathConstraints defaultConstraints = new PathConstraints(
+        Units.feetToMeters(8), 4.0,
+        Units.degreesToRadians(200), Units.degreesToRadians(720));
+
+    public static final PathConstraints fastConstraints = new PathConstraints(
+      Units.feetToMeters(14), 4.0,
+        Units.degreesToRadians(360), Units.degreesToRadians(720));
+  }
+
+  public static final class FieldPoses {
+
+    public static final double[] fieldSize = {17.55, 8.05};
+    // Wall thickness is 0.051
+    public static final Pose2d blueCenterOfReef = new Pose2d(4.487, 4.025, new Rotation2d()); // blue
+    public static final Pose2d redCenterOfReef = new Pose2d(13.065, 4.025, new Rotation2d()); // red
+
+    public static final List<Pose2d> blueReefPoses = new ArrayList<Pose2d>(){{
+      add(new Pose2d(2.890, 4.025, new Rotation2d(Units.degreesToRadians(0))));
+      add(new Pose2d(3.689, 2.642, new Rotation2d(Units.degreesToRadians(60.0))));
+      add(new Pose2d(5.285, 2.642, new Rotation2d(Units.degreesToRadians(120.0))));
+      add(new Pose2d(6.087, 4.025, new Rotation2d(Units.degreesToRadians(180.0))));
+      add(new Pose2d(5.285, 5.408, new Rotation2d(Units.degreesToRadians(240.0))));
+      add(new Pose2d(3.689, 5.408, new Rotation2d(Units.degreesToRadians(300.0))));
+    }};
+
+    public static final List<Pose2d> redReefPoses = new ArrayList<Pose2d>(){{
+      add(new Pose2d(11.466, 4.025, new Rotation2d(Units.degreesToRadians(0))));
+      add(new Pose2d(12.265, 2.642, new Rotation2d(Units.degreesToRadians(60.0))));
+      add(new Pose2d(13.861, 2.642, new Rotation2d(Units.degreesToRadians(120.0))));
+      add(new Pose2d(14.663, 4.025, new Rotation2d(Units.degreesToRadians(180.0))));
+      add(new Pose2d(13.861, 5.408, new Rotation2d(Units.degreesToRadians(240.0))));
+      add(new Pose2d(12.265, 5.408, new Rotation2d(Units.degreesToRadians(300.0))));
+    }};
+
+    public static final double Offset = 0.165;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ReefCentering.java
@@ -0,0 +1,113 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import java.util.List;
+import java.util.Set;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.IdealStartingState;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.Constants.FieldPoses;
+
+
+
+public class ReefCentering {
+
+    private final CommandSwerveDrivetrain drivetrain;
+    private Pose2d nearestReefSide = new Pose2d();
+
+    public enum Side{
+        Left,
+        Middle,
+        Right
+    }
+
+    public ReefCentering(CommandSwerveDrivetrain drivetrain){
+        this.drivetrain = drivetrain;
+    }
+
+    public Pose2d calculateNearestSide(){
+        if(VisionSubsystem.isRedAlliance()){
+            return drivetrain.getPose().nearest(FieldPoses.redReefPoses);
+        }
+        else{
+            return drivetrain.getPose().nearest(FieldPoses.blueReefPoses);
+        }
+    }
+
+    private Pose2d calculatePath(Side side){
+        double x = nearestReefSide.getX();
+        double y = nearestReefSide.getY();
+        double rot = nearestReefSide.getRotation().getRadians();
+        //tune value with more testing, rotational offset 
+        rot += Math.toRadians(0);
+
+        switch (side) {
+            case Left:
+                x -= FieldPoses.Offset * Math.sin(rot);
+                y += FieldPoses.Offset * Math.cos(rot);
+            break;
+            case Right:
+                x += FieldPoses.Offset * Math.sin(rot);
+                y -= FieldPoses.Offset * Math.cos(rot);
+            break;
+            case Middle:
+               default:
+
+            break;
+        }
+
+        Pose2d targetPose = new Pose2d(x, y, new Rotation2d(rot));
+        return targetPose;
+
+    }  
+
+    public boolean haveConditionsChanged(){
+        Pose2d nearSide = calculateNearestSide();
+        
+        if(nearSide != nearestReefSide){
+            return true;
+        }
+        return false;
+    }
+
+    private Command getPathFromWaypoint(Pose2d waypoint){
+        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+            new Pose2d(drivetrain.getPose().getTranslation(), getPathVelocityHeading(drivetrain.getFieldVelocity(), waypoint)), waypoint);
+        
+        if(waypoints.get(0).anchor().getDistance(waypoints.get(1).anchor())< 0.01 ){
+            return Commands.print("Auto alignment too close to desired position to continue");
+        }
+
+        PathPlannerPath path = new PathPlannerPath(
+            waypoints, 
+            Constants.PathPlannerConstants.defaultConstraints, 
+            //might have to add these methods
+            new IdealStartingState(drivetrain.getVelocityMagnitude(), drivetrain.getHeading()), 
+            new GoalEndState(0.0, waypoint.getRotation()));
+        
+        path.preventFlipping = true;
+
+        return AutoBuilder.followPath(path);
+    }
+
+    public Command createPathCommand(Side side){
+        return Commands.defer(()-> {
+            nearestReefSide = calculateNearestSide();
+            Pose2d align = calculatePath(side);
+            Command pathCommand = getPathFromWaypoint(align);
+            return pathCommand;
+        }, Set.of(drivetrain));
+    }
+}
